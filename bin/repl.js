a16eb2c501f56fa60bf555baec9628503f4ea9e2
Andrey Sidorov <sidorares@yandex.com>
2015-04-29 14:17:01 +1000

    use -a key for host parameter #11

--- bin/repl.js
+++ bin/repl.js
@@ -6,7 +6,7 @@ var ChromeREPL = require("../index.js");
 program
   .version('0.0.1')
   .option('-p, --port <port>', 'specify the port [9222]', Number, 9222)
-  .option('-h, --host <host>', 'developer tools host [localhost]', String, 'localhost')
+  .option('-a, --host <host>', 'developer tools host address [localhost]', String, 'localhost')
   .parse(process.argv);
 
 var repl = new ChromeREPL();
