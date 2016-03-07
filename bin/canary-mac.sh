c22f714d1b0e6e80d52af6d35fd2ac0dcd207dbd
Andrey Sidorov <andrey.sidorov@digital-logic.com.au>
2016-03-07 12:23:31 +1100

    add switch to automatically start chrome

--- bin/canary-mac.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --remote-debugging-port=9222
--- bin/repl.js
+++ bin/repl.js
@@ -4,10 +4,54 @@ var program = require('commander');
 var ChromeREPL = require("../index.js");
 
 program
-  .version('0.0.1')
+  .version(require('../package.json').version)
   .option('-p, --port <port>', 'specify the port [9222]', Number, 9222)
   .option('-a, --host <host>', 'developer tools host address [localhost]', String, 'localhost')
+  .option('-s, --start', 'start chrome auromatically and wait for debugger', Boolean, false)
+  .option('-c, --canary', 'prefer canary over stable chrome', Boolean, false)
   .parse(process.argv);
 
 var repl = new ChromeREPL();
-repl.start(program);
+
+if (program.start) {
+  startChrome(function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    repl.start(program);
+  });
+} else {
+  repl.start(program);
+}
+
+function startChrome(cb) {
+  var MAX_TIMEOUT = 5000;
+  var startTime = new Date();
+  var chrome_runner = require('node-chrome-runner');
+  var versions;
+  if (program.canary)
+    versions = [chrome_runner.chromePaths.canary];
+  var c = chrome_runner.runChrome({
+    versions: versions,
+    args:['--remote-debugging-port=' + program.port]
+  });
+  var net = require('net');
+  process.stdout.write('Waiting for chrome to start: ');
+  function tryConnect() {
+    var conn = net.connect(program.port);
+    conn.on('error', function() {
+      process.stdout.write('.')
+      if (new Date() - startTime < MAX_TIMEOUT)
+        setTimeout(tryConnect, 300);
+      else
+        cb(new Error('timed out waiting for remote debugger available on port ' + program.port));
+    });
+    conn.on('connect', function() {
+      console.log('');
+      conn.end();
+      cb();
+    });
+  }
+  tryConnect();
+}
